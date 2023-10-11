20595cb0ddc7c48ae4fad6b15def29b083cb4be5
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-11 22:26:30 +0400

    Practice4.py

--- Chapter 1- Getting Started with Python/Practice/Practice4.py
+++ Chapter 1- Getting Started with Python/Practice/Practice4.py
@@ -1,1 +1,7 @@
 #Compute the area of triangle
+
+print("Area of a triangle is base*height/2")
+b=int(input("Enter the Base of the triangle: "))
+h=int(input("Enter the height of the triangle: "))
+area=b*h/2
+print("The Area of the given triangle is ",area,".")
