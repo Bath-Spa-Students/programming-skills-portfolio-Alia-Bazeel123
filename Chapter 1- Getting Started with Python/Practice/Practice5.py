daa6a23f769df357b61cc7c57bab668fe8b6a10c
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-11 22:33:25 +0400

    Practice5.py

--- Chapter 1- Getting Started with Python/Practice/Practice5.py
+++ Chapter 1- Getting Started with Python/Practice/Practice5.py
@@ -1,1 +1,9 @@
 # Write four strings in different variables and print the output as one string. (String Concatenation)
+
+x="We"
+y="are"
+z="using"
+w="Python"
+print(x+y+z+w)
+print(x+" "+y+" "+z+" "+" "+w)
+print(x,"",y,"",z,"",w)
