3e9a701d3fd573ba401c1fff0c64658f696cbda1
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-12 09:43:29 +0400

    Exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -1,1 +1,6 @@
-print(datetime.now(tz=None))
+#Write a Python program to display the current date and time.
+
+import datetime
+now=datetime.datetime.now()
+print("Current date and time: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
