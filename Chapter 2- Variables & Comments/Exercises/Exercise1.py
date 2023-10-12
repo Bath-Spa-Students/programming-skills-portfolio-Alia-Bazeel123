a715ae5368c7db500c4091efb4e62aaf2d518512
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-12 13:32:46 +0400

    Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,7 @@
+'''Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message.'''
+
+a="We are BSU Students."
+print(a)
+a="Python is the programming language we learn."
