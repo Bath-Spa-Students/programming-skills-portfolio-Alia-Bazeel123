0fb7d2fc92af35feda6eac87305269b6eaa7902f
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-21 23:22:32 +0400

    Exercise3.py

--- Chapter 3- Data Structures/Exercises/Exercise1.py
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -2,6 +2,7 @@
 Print each person's name by accessing each element in the list, one at a time.'''
 
 names = ["Angela", "Aimee", "Saniya"]
+
 print(names[0])
 print(names[1])
 print(names[2])
---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+'''Think of your favorite mode of transportation, 
+such as a motorcycle or a car, and make a list that stores several examples. 
+Use your list to print a series of statements about these items, 
+such as “I would like to own a Honda motorcycle.” '''
+
+fav_transp = ["car", "aeroplane", "train", "bus"]
+
+print ("For a long distance travel,",fav_transp[2],"is not always a cheaper and faster option than",fav_transp[1],'.')
+print ("The speed of a", fav_transp[0], "is 40% more than that of a", fav_transp[3], '.')
