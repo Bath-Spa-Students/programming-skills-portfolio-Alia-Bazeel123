397c5bc40dd0429b69ed0f6d3a8366975cc2d3b0
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-10-21 22:51:07 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,14 @@
+'''Start with the list you used in Exercise 1, but instead of just 
+printing each person's name, print a message to them. 
+The text of each message should be the same, 
+but each message should be personalized with the person's name.'''
+
+names = ["Angela", "Aimee", "Saniya"]
+
+a = "How are you, " + names[0] + "?"
+b = "How are you, " + names[1] + "?"
+c = "How are you, " + names[2] + "?"
+
+print(a)
+print(b)
+print(c)
