be11d205961b6e14aa726ddb3ff5f98cff8f5094
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-05 16:24:54 +0400

    Practice4.py

---
+++ Chapter 2- Variables & Comments/Practice/Practice4.py
@@ -0,0 +1,7 @@
+'''Write a python program that stores an integer and string value to variables x and y. 
+Print the type of variable x and y. '''
+
+x = 100                   # Storing values to the variables.
+y = "Alia"
+print(type(x))            # Retrieving the data type of the values stored in the variables.
+print(type(y))
