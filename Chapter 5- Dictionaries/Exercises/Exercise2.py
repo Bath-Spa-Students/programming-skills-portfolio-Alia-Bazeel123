1c28498a22efba1b8162d85003433bc70ce8b8ce
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-05 18:49:47 +0400

    Exercise2.py

--- Chapter 5- Dictionaries/Exercises/Exercise2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -7,4 +7,16 @@ However, to avoid confusion, let's call it a glossary.
 * Print each word and its meaning as neatly formatted output. 
   You might print the word followed by a colon and then its meaning, or print 
   the word on one line and then print its meaning indented on a second line. 
-  Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.'''
+  Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+'''
+glossary = {"Comment": "The line that is ignored by the interpreter.", 
+            "print()": "Displays the messsege.", 
+            "input()": "Takes the input/values from the user.", 
+            "string": "A collection of alphabets, words or other characters enclosed in quotation marks.", 
+            "in/not in": "Determine if a given value is or isn't part of a collection of values."}
+
+print("\n Comment: ", glossary["Comment"])
+print("\n print(): ", glossary["print()"])
+print("\n input(): ", glossary["input()"])
+print("\n string: ", glossary["string"])
+print("\n in/not in: ", glossary["in/not in"])
