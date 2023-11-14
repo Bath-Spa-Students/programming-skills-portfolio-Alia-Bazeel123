6d170c84bf4437c7acaf3d17e754824697b2ff6b
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-14 18:47:17 +0400

    Exercise3.py

--- Chapter 5- Dictionaries/Exercises/Exercise3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -9,7 +9,12 @@ glossary = {"Comment": "The line that is ignored by the interpreter.",
             "print()": "Displays the messsege.", 
             "input()": "Takes the input/values from the user.", 
             "string": "A collection of alphabets, words or other characters enclosed in quotation marks.", 
-            "in/not in": "Determine if a given value is or isn't part of a collection of values."}
+            "in/not in": "Determine if a given value is or isn't part of a collection of values.", 
+			"Iterators": "An object that contains a countable number of values.", 
+			"while Loop": "We can execute a set of statements as long as a condition is true.", 
+			"for Loop": "Used for iterating over a sequence.", 
+			"Variables": "Containers for storing data values.", 
+			"append()": "To add an item to the end of the list.", }
 
 for word, definition in glossary.items():
 	print (word +": " + definition + '\n')
