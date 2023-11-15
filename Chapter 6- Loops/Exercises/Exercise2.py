892867107bf34c2dd4ab393f378d408d7c0bb8be
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-15 21:05:28 +0400

    Exercise2.py

--- Chapter 6- Loops/Exercises/Exercise2.py
+++ Chapter 6- Loops/Exercises/Exercise2.py
@@ -4,3 +4,10 @@ If a person is under the age of 3, the ticket is free; if they are between 3 and
 and if they are over age 12, the ticket is $15. 
 Write a loop in which you ask users their age, and then tell them the cost of their movie ticket
 '''
+age = int (input("Enter your age: "))
+if age < 3:
+    print ("The ticket is free.")
+elif age < 12:
+    print ("The ticket is $10.")
+else:
+    print ("The ticket is $15.")
