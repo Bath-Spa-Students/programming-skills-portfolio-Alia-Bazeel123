3929ee7b91b2b785facccc75219709b71fda8fa4
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-15 15:28:32 +0400

    Exercise4.py

--- Chapter 5- Dictionaries/Exercises/Exercise4.py
+++ Chapter 5- Dictionaries/Exercises/Exercise4.py
@@ -7,17 +7,20 @@ One key-value pair might be 'nile': 'egypt'.
 
 * Use a loop to print the name of each country included in the dictionary.
 '''
-river_country = {'nile': 'egypt', 
-                 "Volga River": "Russia", 
-                 "Yangtze River": "China", 
-                 "Mississippi River": "United States"}
+river_country = {
+    'nile': 'egypt', 
+    "Volga River": "Russia", 
+    "Yangtze River": "China", 
+    "Mississippi River": "United States"}
 
 for river, country in river_country.items():                     #a sentence about each river
     print ("The " + river.title() + " runs through " + country.title() + ".")
 
 print ("\n Names of Rivers included in the dictionary: ")
-for river in river_country.keys():
+for river in river_country.keys():                #name of each river included in the dictionary
     print ("-> " + river.title())
 
-                  #name of each river included in the dictionary
-  #  print("- " + river.title())
+print ("\n Names of Rivers included in the dictionary: ")
+for country in river_country.values():                #name of each country included in the dictionary
+    print ("-> " + country.title())
+    
