a65f9d7e0ee3140c58d825a6ef40e42359231131
Alia bint Bazeel <aliabazeel123@gmail.com>
2023-11-15 21:09:28 +0400

    Exercise3.py

--- Chapter 6- Loops/Exercises/Exercise3.py
+++ Chapter 6- Loops/Exercises/Exercise3.py
@@ -2,18 +2,13 @@
 (To end the loop, press ctrl-C or close the window displaying the output.)
 '''
 
-
-'''
-while True:
+while True:                                         # infinity loop with break
     n = int(input('Give me an integer: '))
-
     if n == 0:
         break
-
     print(str(n) + '*' + str(n) + '=' + str(n*n))
+print('done')
 
-print('done')'''
-
-i = 0
+i = 0                                                # loop that never ends
 while i < 10:
     print(i)
